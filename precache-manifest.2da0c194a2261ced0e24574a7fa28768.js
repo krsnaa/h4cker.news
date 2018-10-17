ac266b9a0959660c16c66e5791edbddbffddf841
krsnaa <krsnaa@gmail.com>
2018-10-16 21:43:27 -0700

    Updates

---
+++ precache-manifest.2da0c194a2261ced0e24574a7fa28768.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "208cf2f5f885e9584a28",
+    "url": "/h4cker.news/static/js/runtime~main.208cf2f5.js"
+  },
+  {
+    "revision": "866c9085ed62841598e0",
+    "url": "/h4cker.news/static/js/main.866c9085.chunk.js"
+  },
+  {
+    "revision": "74194b54de4ef37da279",
+    "url": "/h4cker.news/static/js/1.74194b54.chunk.js"
+  },
+  {
+    "revision": "866c9085ed62841598e0",
+    "url": "/h4cker.news/static/css/main.63a6fbd9.chunk.css"
+  },
+  {
+    "revision": "559b85b7d4d38118df4dd6df09497bc8",
+    "url": "/h4cker.news/index.html"
+  }
+];
